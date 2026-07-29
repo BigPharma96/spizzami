62db7140ed984c3cf4a8e99363e4ec51ecd58955
BigPharma96 <92854657+BigPharma96@users.noreply.github.com>
2026-07-29 22:47:13 +0200

    Update CLIENT_ID in config.js

--- config.js
+++ config.js
@@ -2,4 +2,4 @@
    It looks like: 1234567890-abcdefg.apps.googleusercontent.com
    Step-by-step instructions are in README.md. */
 
-var CLIENT_ID = '957583130888-o821otfdn9t22gr0lg35v3upsm1478dl.apps.googleusercontent.com';
+var CLIENT_ID = '134593623094-oeski3794k48586rdvr4hae4lvjaotve.apps.googleusercontent.com';
